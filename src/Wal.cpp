5e6677af497abc82b26c215e972d7799d7170af6
Michael Franks <mffranks@pm.me>
2026-08-04 20:52:03 +0100

    Created a system to save and write a log to file

--- src/Wal.cpp
+++ src/Wal.cpp
@@ -4,7 +4,53 @@
 
 #include "Wal.h"
 
-Wal::Wal(std::string& path)
-{
+#include <fstream>wha
+#include <valarray>
+#include <vector>
 
+
+std::unordered_map<std::string, std::string> Wal::read() const
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    std::unordered_map<std::string, std::string> data;
+    std::ifstream file(path);
+
+    std::string line;
+    while (getline(file, line))
+    {
+        // key;data
+        std::pair<std::string, std::string> pair = splitLine(line);
+        data.try_emplace(pair.first, pair.second);
+    }
+    file.close();
+    return data;
+}
+
+void Wal::write(const std::unordered_map<std::string, std::string>& data) const
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    std::string temp_path = path + ".tmp";
+    {
+        std::ofstream log(temp_path);
+        for (auto& line : data)
+        {
+            log << line.first << DELIM << line.second << "\n";
+        }
+    }
+    std::rename(temp_path.c_str(), path.c_str());
+}
+
+std::pair<std::string, std::string> Wal::splitLine(const std::string& line)
+{
+    size_t pos = line.find(DELIM);
+
+    if (pos == std::string::npos)
+    {
+        return std::make_pair(line, "");
+    }
+    std::string first = line.substr(0, pos);
+    std::string second = line.substr(pos + 1);
+    return {first, second};
 }
--- src/Wal.h
+++ src/Wal.h
@@ -5,21 +5,40 @@
 #ifndef WAL_H
 #define WAL_H
 
+#include <mutex>
 #include <string>
+#include <unordered_map>
 
 
-class Wal {
+class Wal
+{
 public:
-    Wal(std::string &path);
-    void save();
-    void load();
+    Wal() = default;
+    ~Wal() = default;
+
+    /**
+     * system to reading in the data into the registry on restart
+     * @return Returns the data that was stored from last lower cycle
+     */
+    [[nodiscard]] std::unordered_map<std::string, std::string> read() const;
+
+    /**
+     * creates a data coppy of in-memory storage.
+     * @param data data writing to application
+     */
+    void write(const std::unordered_map<std::string, std::string>& data) const;
 
 private:
-    bool read();
-    bool write();
+    /**
+     * line to break into 2 parts
+     * @param line current line to process
+     * @return line broken into 2.
+     */
+    static std::pair<std::string, std::string> splitLine(const std::string& line);
 
     std::string path = "log.txt";
-
+    static constexpr char DELIM = '\x1F';
+    mutable std::mutex mutex_;
 };
 
 
